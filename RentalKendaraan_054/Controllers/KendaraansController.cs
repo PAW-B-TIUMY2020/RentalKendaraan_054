d99601db382686b62e686cc771c75ab0e82619a4
Muhammad Ferdy <ferdyzecter@gmail.com>
2020-11-24 23:50:39 +0700

    Next

--- RentalKendaraan_054/Controllers/KendaraansController.cs
+++ RentalKendaraan_054/Controllers/KendaraansController.cs
@@ -19,7 +19,7 @@ namespace RentalKendaraan_054.Controllers
         }
 
         // GET: Kendaraans
-        public async Task<IActionResult> Index(string ktsd, string searchString)
+        public async Task<IActionResult> Index(string ktsd, string sortOrder, string searchString, string currentFilter, int? pageNumber)
         {
             //buat list menyimpan ketersediaaan
             var ktsdList = new List<string>();
@@ -45,7 +45,36 @@ namespace RentalKendaraan_054.Controllers
             {
                 menu = menu.Where(s => s.NoPolisi.Contains(searchString) || s.NamaKendaraan.Contains(searchString) || s.NoStnk.Contains(searchString));
             }
-            return View(await menu.ToListAsync());
+            ViewData["CurrentSort"] = sortOrder;
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewData["CurrentFilter"] = searchString;
+            int pageSize = 5;
+            //untuk sorting
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    menu = menu.OrderByDescending(s => s.NamaKendaraan);
+                    break;
+                case "Date":
+                    menu = menu.OrderBy(s => s.IdJenisKendaraanNavigation.NamaJenisKendaraan);
+                    break;
+                case "date_desc":
+                    menu = menu.OrderByDescending(s => s.IdJenisKendaraanNavigation.NamaJenisKendaraan);
+                    break;
+                default: //nama ascending
+                    menu = menu.OrderBy(s => s.NamaKendaraan);
+                    break;
+            }
+            return View(await PaginatedList<Kendaraan>.CreateAsync(menu.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Kendaraans/Details/5
